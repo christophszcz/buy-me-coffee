edbc4d0c4624fecf723302842c01a33dd5d2c3e2
Chris Szczechowicz <info@cjszczechowicz.com>
2023-06-29 23:52:55 -0400

    Setup boilerplate code for one time donations

--- server/server.js
+++ server/server.js
@@ -3,8 +3,16 @@ require('dotenv').config();
 const express = require('express');
 const app = express;
 
-app.request(express.json());    // to parse the body of the request
+app.use(express.json()); 
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const donationItems = new Map([
+    ['1', {priceInCents: '100', name: 'Donation $1'}],
+    ['2', {priceInCents: '500', name: 'Donation $5'}],
+    ['3', {priceInCents: '1000', name: 'Donation $10'}],
+    ['4', {priceInCents: '2000', name: 'Donation $20'}],
+]);
+
 
 app.listen(3000, () => console.log('Server running on port 3000'));
