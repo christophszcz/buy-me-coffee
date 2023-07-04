6455ed42d581d1cb0f3b021aa6307761c232e84b
Chris Szczechowicz <info@cjszczechowicz.com>
2023-07-04 03:22:05 -0400

    Add origin to CORS dev setup

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+app.use(cors({ origin: 'http://localhost:3000'}));
 
 app.use(express.json()); 
 
