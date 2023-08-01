6d3f3af4caa2c5859f46cc40d9ae893aa02e06ac
Chris Szczechowicz <info@cjszczechowicz.com>
2023-07-31 23:13:56 -0400

    Setup the line_items data

--- server/server.js
+++ server/server.js
@@ -10,10 +10,10 @@ app.use(express.json());
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const donationItems = new Map([
-    ['1', {priceInCents: '100', name: 'Donation $1'}],
-    ['2', {priceInCents: '500', name: 'Donation $5'}],
-    ['3', {priceInCents: '1000', name: 'Donation $10'}],
-    ['4', {priceInCents: '2000', name: 'Donation $20'}],
+    [1, {priceInCents: '100', name: 'Donation $1'}],
+    [2, {priceInCents: '500', name: 'Donation $5'}],
+    [3, {priceInCents: '1000', name: 'Donation $10'}],
+    [4, {priceInCents: '2000', name: 'Donation $20'}],
 ]);
 
 app.listen(3000, () => console.log('Server running on port 3000'));
@@ -23,8 +23,18 @@ app.post('/create-checkout-session', async (req, res) => {
         const session = await stripe.checkout.sessions.create({
             payment_method_types: ['card'],
             mode: 'payment',
-            line_items: req.body.items.map(items => {
-
+            line_items: req.body.items.map(item => {
+                const donationItem = donationItem.get(item.id);
+                return {
+                    price_data: {
+                        currency: 'usd',
+                        product_data: {
+                            name: donationItem.name
+                        },
+                        unit_amount: donationItem.priceInCents
+                    },
+                    quantity: item.quantity
+                }
             }),
             success_url: `${process.env.SERVER_URL}/success.html`,
             cancel_url: `${process.env.SERVER_URL}/cancel.html`,
